62675900e1d97e3b2c6ea0d4e10e6345f0726eaa
sue <sue.song@mail.utoronto.ca>
2020-01-06 14:41:42 -0500

    edits made

--- ex_ans/Q4.R
+++ ex_ans/Q4.R
@@ -1,5 +1,5 @@
 # read-in saved basket dataset for the example
-basket <- readr::read_csv(here::here("dataset", "basket.csv"))
+(basket <- readr::read_csv(here::here("dataset", "basket.csv")))
 
 # Finding the names of fruits and veggies whose names do not start with "c" using the startsWith() function. 
 basket[!startsWith(basket$fruits, "c"), "fruits"]
